dbaab3e7e550d0c9fd051f52fa742624f941a96e
justfunX99 <rayhsu@pollex.com.tw>
2023-12-06 20:39:28 +0800

    add: swagger api call with auto Authorizaion Bear token

--- src/main/java/com/justfun/config/SpringFoxConfig.java
+++ src/main/java/com/justfun/config/SpringFoxConfig.java
@@ -1,10 +1,19 @@
 package com.justfun.config;
 
+import java.util.Collections;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.spi.service.contexts.SecurityContext;
+
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.service.SecurityScheme;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -13,13 +22,30 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SpringFoxConfig {
 
-    @Bean
-    public Docket api() { 
-        return new Docket(DocumentationType.SWAGGER_2)  
-          .select()                                  
-          .apis(RequestHandlerSelectors.any())              
-          .paths(PathSelectors.any())                          
-          .build();                                           
-    }
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any()).build().securityContexts(securityContext())
+				.securitySchemes(securitySchemes());
+
+	}
+
+	private List<SecurityScheme> securitySchemes() {
+		return Collections.singletonList(new ApiKey("JWT", "Authorization", "header"));
+	}
+
+	private List<SecurityContext> securityContext() {
+		SecurityContext securityContext = SecurityContext.builder().securityReferences(defaultAuth()).build();
+
+		return Collections.singletonList(securityContext);
+	}
+
+	private List<SecurityReference> defaultAuth() {
+		AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+		AuthorizationScope[] arr = new AuthorizationScope[1];
+		arr[0] = authorizationScope;
+
+		return Collections.singletonList(new SecurityReference("JWT", arr));
+	}
 
 }
