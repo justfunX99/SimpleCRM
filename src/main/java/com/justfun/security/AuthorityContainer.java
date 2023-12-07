3baad0fb6e29baf1bb6f99c590fa0f2e03efdde9
justfunX99 <rayhsu@pollex.com.tw>
2023-12-07 23:12:51 +0800

    add: 增加角色的客戶資料維護相關權限

--- src/main/java/com/justfun/security/AuthorityContainer.java
+++ src/main/java/com/justfun/security/AuthorityContainer.java
@@ -9,23 +9,36 @@ import org.springframework.stereotype.Component;
 
 @Component("A")
 public class AuthorityContainer {
+	// company
 	public static final String VIEW_COMPANY = "VIEW_COMPANY";
 	public static final String CREATE_COMPANY = "CREATE_COMPANY";
 	public static final String MODIFY_COMPANY = "MODIFY_COMPANY";
 	public static final String DELETE_COMPANY = "DELETE_COMPANY";
+	
+	// client
+	public static final String VIEW_CLIENT = "VIEW_CLIENT";
+	public static final String CREATE_CLIENT = "CREATE_CLIENT";
+	public static final String MODIFY_CLIENT = "MODIFY_CLIENT";
+	public static final String DELETE_CLIENT = "DELETE_CLIENT";
 	
 	public List<String> getAuthoritie(RoleEnum role){
 		Stream<String> resStream = Stream.empty();
 		
 		switch(role) {
 			case SUPER_USER:
-				resStream = Stream.of(VIEW_COMPANY, CREATE_COMPANY, MODIFY_COMPANY, DELETE_COMPANY);
+				resStream = Stream.of(VIEW_COMPANY, CREATE_COMPANY, MODIFY_COMPANY, DELETE_COMPANY,
+									  VIEW_CLIENT, CREATE_CLIENT, MODIFY_CLIENT, DELETE_CLIENT
+									 );
 				break;
 			case MANAGER:
-				resStream = Stream.of(VIEW_COMPANY, MODIFY_COMPANY, DELETE_COMPANY);
+				resStream = Stream.of(VIEW_COMPANY, MODIFY_COMPANY, DELETE_COMPANY,
+									  VIEW_CLIENT, MODIFY_CLIENT, DELETE_CLIENT
+									 );
 				break;
 			case OPERATOR:
-				resStream = Stream.of(VIEW_COMPANY, CREATE_COMPANY);
+				resStream = Stream.of(VIEW_COMPANY, CREATE_COMPANY,
+									  VIEW_CLIENT, CREATE_CLIENT
+								     );
 				break;
 			default:
 		}
