574a492511e0978b708c2f6c133e29deac795358
justfunX99 <rayhsu@pollex.com.tw>
2023-12-12 00:56:58 +0800

    add: 3種角色權限帳號，對客戶維護API與公司維護API的操作權限，單元測試

--- src/test/java/com/justfun/rest/BaseTest.java
+++ src/test/java/com/justfun/rest/BaseTest.java
@@ -1,5 +1,7 @@
 package com.justfun.rest;
 
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.apache.commons.lang3.StringUtils;
@@ -8,10 +10,12 @@ import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import com.google.gson.Gson;
+import com.justfun.dto.CompanyDTO;
 import com.justfun.vo.AccountVO;
 
 public class BaseTest {
@@ -52,5 +56,100 @@ public class BaseTest {
     protected final MockHttpServletRequestBuilder addJWTToken(MockHttpServletRequestBuilder builder, String token) {
     	return builder.header("Authorization", "Bearer " + token);
     }
+    
+    // for company
+    protected void createCompanyAccess(String token, ResultMatcher matcher, boolean throwException) throws Exception {
+    	MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.post("/api/companies/add")
+																	  .contentType(MediaType.APPLICATION_JSON)
+																	  .content("{}");
+    	
+    	performAndExpect(token, builder, matcher, throwException);
+    }
+
+	protected void findCompanyByIdAccess(String token, ResultMatcher matcher, boolean throwException) throws Exception {
+	   	MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.get("/api/companies/999");
+    	performAndExpect(token, builder, matcher, throwException);
+    }
+    
+    protected void findAllCompaniesAccess(String token, ResultMatcher matcher, boolean throwException) throws Exception {
+	   	MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.get("/api/companies");
+    	performAndExpect(token, builder, matcher, throwException);
+    }
+    
+    protected void modifyCompanyAccess(String token, ResultMatcher matcher, boolean throwException) throws Exception {
+    	MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.put("/api/companies/999")
+																	  .contentType(MediaType.APPLICATION_JSON)
+																	  .content("{}");
+    	
+    	performAndExpect(token, builder, matcher, throwException);
+    }
+    
+    protected void deleteCompanyAccess(String token, ResultMatcher matcher, boolean throwException) throws Exception {
+    	MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.delete("/api/companies/999");
+    	performAndExpect(token, builder, matcher, throwException);
+    }
+    
+    // for clients
+    protected void createMultipleClientsAccess(String token, ResultMatcher matcher, boolean throwException) throws Exception {
+    	MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.post("/api/clients/bulkAdd")
+														    		  .contentType(MediaType.APPLICATION_JSON)
+																	  .content("{}");
+    	
+    	performAndExpect(token, builder, matcher, throwException);
+    }
+    
+    protected void createClientAccess(String token, ResultMatcher matcher, boolean throwException) throws Exception {
+    	MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.post("/api/clients/add")
+														    		  .contentType(MediaType.APPLICATION_JSON)
+																	  .content("{}");
+    	
+    	performAndExpect(token, builder, matcher, throwException);
+    }
+    
+    protected void findAllClientsAccess(String token, ResultMatcher matcher, boolean throwException) throws Exception {
+	   	MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.get("/api/clients");
+    	performAndExpect(token, builder, matcher, throwException);
+    }
+    
+    protected void findClientByIdAccess(String token, ResultMatcher matcher, boolean throwException) throws Exception {
+	   	MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.get("/api/clients/999");
+    	performAndExpect(token, builder, matcher, throwException);
+    }
+    
+    protected void modifyClientAccess(String token, ResultMatcher matcher, boolean throwException) throws Exception {
+    	MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.put("/api/clients/999")
+																	  .contentType(MediaType.APPLICATION_JSON)
+																	  .content("{}");
+    	
+    	performAndExpect(token, builder, matcher, throwException);
+    }
+    
+    protected void deleteClientAccess(String token, ResultMatcher matcher, boolean throwException) throws Exception {
+    	MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.delete("/api/clients/999");
+    	performAndExpect(token, builder, matcher, throwException);
+    }
+    
+    /// private /////
+    private void performAndExpect(
+						    		String token,
+						    		MockHttpServletRequestBuilder builder,
+						    		ResultMatcher matcher,
+						    		boolean throwException
+    ) throws Exception {
+    	 assertTrue(!StringUtils.isEmpty(token));
+    	
+    	 addJWTToken(builder, token);
+    	
+    	if(matcher != null) {
+    		mvc.perform(builder).andExpect(matcher);
+    	}
+    	else if(throwException) {
+    		// 代表有權限
+            assertThrows(Exception.class, () -> {
+            	mvc.perform(builder);
+            });
+    	}
+		
+	}
     
 }
---
+++ src/test/java/com/justfun/rest/SuperUserAATest.java
@@ -0,0 +1,86 @@
+package com.justfun.rest;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class SuperUserAATest extends BaseTest {
+	
+	private static Boolean _init = false;
+	
+	private static String _token;
+	
+	@BeforeEach
+	private void init() throws Exception {
+		if(!_init) {
+			_token = loginAndGetToken("ray", "ray");
+			_init = true;
+		}
+	}
+	
+	// for companies
+	@Test
+	public void create_company_test() throws Exception {
+		createCompanyAccess(_token, null, true);
+	}
+	
+	@Test
+	public void find_company_by_id_test() throws Exception {
+		findCompanyByIdAccess(_token, status().isOk(), false);
+	}
+	
+	@Test
+	public void find_all_company_test() throws Exception {
+		findAllCompaniesAccess(_token, status().isOk(), false);
+	}
+	
+	@Test
+	public void modify_company_test() throws Exception {
+		modifyCompanyAccess(_token, null, true);
+	}
+	
+	@Test
+	public void delete_company_test() throws Exception {
+		deleteCompanyAccess(_token, null, true);
+	}
+	
+	// for clients
+	@Test
+	public void create_multiple_clients_test() throws Exception {
+		createMultipleClientsAccess(_token, null, true);
+	}
+	
+	@Test
+	public void create_client_test() throws Exception {
+		createClientAccess(_token, null, true);
+	}
+	
+	@Test
+	public void find_client_by_id_test() throws Exception {
+		findClientByIdAccess(_token, status().isOk(), false);
+	}
+	
+	@Test
+	public void find_all_clients_test() throws Exception {
+		findAllClientsAccess(_token, status().isOk(), false);
+	}
+	
+	@Test
+	public void modify_client_test() throws Exception {
+		modifyClientAccess(_token, null, true);
+	}
+	
+	@Test
+	public void delete_client_test() throws Exception {
+		deleteClientAccess(_token, null, true);
+	}
+	
+}
